f74bb881c040a52bb3929bbf4e8eca26462573a6
Ritik Raj <rritik345@gmail.com>
2026-06-02 12:57:25 +0530

    all files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer.jsx'
+
+function App() {
+ 
+
+  return (
+    <div className='App'>
+      <PathfindingVisualizer />
+    </div>
+  )
+}
+
+export default App
